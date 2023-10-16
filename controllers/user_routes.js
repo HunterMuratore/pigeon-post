a92ab30a0617d687b75bf738f6e1b5c218cb845b
Hunter <muratoreh@gmail.com>
2023-10-16 14:33:42 -0400

    set up POST route for register

--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -4,12 +4,19 @@ const User = require('../models/User');
 
 // POST request route that retrieves the registration form data and creates a new user in the db
 // The route will respond with a message of 'User added successfully' 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const data = req.body;
 
-    User.create(data)
-        .then(newUser => res.send({ message: 'User added successfully!', user: newUser }))
-        .catch(err => console.log(err));
+    // User.create(data)
+    //     .then(newUser => res.send({ message: 'User added successfully!', user: newUser }))
+    //     .catch(err => console.log(err));
+
+    try {
+        await User.create(data);
+    
+        res.json({ message: 'User added successfully!', user: newUser});
+    
+      } catch (err) { res.status(400).json(err) };
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const view_routes = require('./controllers/view_routes');
+const user_routes = require('./controllers/user_routes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
@@ -12,5 +13,6 @@ app.use(express.static('public'));
 
 // Open our routes at the root level
 app.use('/', view_routes);
+app.use('/auth', user_routes);
 
 app.listen(PORT, () => console.log('Server started on port', PORT));
