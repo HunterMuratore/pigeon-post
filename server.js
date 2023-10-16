fdca737ca34472ad9fa95696d28daae58c5a7cc8
Hunter <muratoreh@gmail.com>
2023-10-16 11:33:43 -0400

    middleware update

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
-// Open the static channel for our browser assets - ie. express.static on the public folder
-express.static('./public')
-
-// Load our view routes at the root level '/'
+// Open Middleware channels
+app.use(express.static('./public'));
 app.use('/', view_routes);
 
-// Start the server and log the port that it started on
 app.listen(PORT, () => console.log('Server started on port', PORT));
